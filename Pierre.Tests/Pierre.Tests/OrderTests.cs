a095bdfc1980dc04cdf333ddddcc17b73ae3a3a7
Jeff Kim <kim2jy@hotmail.com>
2020-07-24 11:17:16 -0700

    Add eighth failing test to set an Order Title

--- Pierre.Tests/Pierre.Tests/OrderTests.cs
+++ Pierre.Tests/Pierre.Tests/OrderTests.cs
@@ -19,5 +19,19 @@ namespace Pierre.Tests
       Order newOrder = new Order("Croissants", "Test Order", 200, "2020-07-24");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+
+    [TestMethod]
+    public void GetTitle_ReturnTitle_String()
+    {
+      string title = "Croissants";
+      string description = "Test Order";
+      int price = 200;
+      string date = "2020-07-24";
+      Order newOrder = new Order(title, description, price, date);
+
+      string result = newOrder.Title;
+
+      Assert.AreEqual(title, result);
+    }
   }
 }
