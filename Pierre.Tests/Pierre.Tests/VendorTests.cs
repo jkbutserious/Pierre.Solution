a287a2eeb109b9cfb9ab8557785cb9d3a90c0935
Jeff Kim <kim2jy@hotmail.com>
2020-07-24 09:45:47 -0700

    Add first passing test for Vendor constructor

--- Pierre.Tests/Pierre.Tests/VendorTests.cs
+++ Pierre.Tests/Pierre.Tests/VendorTests.cs
@@ -2,3 +2,22 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Pierre.Models;
 using System.Collections.Generic;
 using System;
+
+namespace Pierre.Tests
+{
+  [TestClass]
+  public class VendorTest : IDisposable
+  {
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("Suzie's Cafe", "Test Vendor");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
+}
---
+++ Pierre/Models/Vendor.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+
+namespace Pierre.Models
+{
+  public class Vendor
+  {
+    private static List<Vendor> _instances = new List<Vendor> {};
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Id { get; }
+    // public List<Order> Orders { get; set; } //Will add when Order class is created
+
+    public Vendor(string vendorName, string description)
+    {
+      Name = vendorName;
+      _instances.Add(this);
+      Id = _instances.Count;
+      // Orders = new List<Order>{}; //Will add when Order class is created
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+  }
+}
